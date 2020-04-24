f8d7e77ca70e37078ba4e786c6a53b4f87a1aa5f
adev22 <Adevsaputra3@gmail.com>
2020-04-23 21:11:38 -0400

    Updates

---
+++ precache-manifest.c1a0de42d8879575642a9a9bee89f488.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90186cb395cff2fb1d80fb75cb4293b3",
+    "url": "/covidapp/index.html"
+  },
+  {
+    "revision": "5d65fe099261a2fcffd3",
+    "url": "/covidapp/static/css/main.13c8a689.chunk.css"
+  },
+  {
+    "revision": "5c61f3d77c85d75cc589",
+    "url": "/covidapp/static/js/2.cb03dd96.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidapp/static/js/2.cb03dd96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d65fe099261a2fcffd3",
+    "url": "/covidapp/static/js/main.fef07ae6.chunk.js"
+  },
+  {
+    "revision": "5128a54df23fed013939",
+    "url": "/covidapp/static/js/runtime-main.6ef7c87f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidapp/static/media/image.d7265326.png"
+  }
+]);
